d1f66eceaea071b00167157ab91d694693c2d0ba
Andre Praeiro <andrepraeiro@gmail.com>
2016-05-24 17:54:11 -0400

    layout da tela do estabelecimento

--- app/src/main/java/com/praeiro/marketprice/com/praeiro/marketprice/estabelecimento/Estabelecimento.java
+++ app/src/main/java/com/praeiro/marketprice/com/praeiro/marketprice/estabelecimento/Estabelecimento.java
@@ -1,7 +1,17 @@
 package com.praeiro.marketprice.com.praeiro.marketprice.estabelecimento;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.praeiro.marketprice.R;
+
 /**
  * Created by Praeiro on 23/05/2016.
  */
-public class Estabelecimento {
+public class Estabelecimento extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.estabelecimento);
+    }
 }
